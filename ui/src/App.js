4dfe527046300d610a5786850831f71b19500cfd
Tracey <tracy.yan2018@gmail.com>
2022-07-29 16:41:48 +1000

    Navebar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,11 @@
-import {Button, Typography} from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import Navbar from "./components/navbar/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <Typography align="center" variant="h1" color={"tomato"}>
-  h1. Heading
-</Typography>
-     <Button endIcon={<FacebookIcon />} variant="outlined" color="error">Beautiful</Button>
-    </div>
+      <>
+      <Navbar/>
+      </>
   );
 }
 
