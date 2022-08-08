4400a8f722b9f639a05917ef718634e02ac18d40
Tracey <tracy.yan2018@gmail.com>
2022-08-08 15:38:23 +1000

    update the card component about typography responsive

--- ui/src/components/card/Card.js
+++ ui/src/components/card/Card.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { Box, CardMedia, CardContent, Typography, Link } from '@mui/material';
+import {
+    createTheme,
+    responsiveFontSizes,
+    ThemeProvider,
+  } from '@mui/material/styles';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 const Card = ({ cardImage }) => {
   return (
@@ -12,18 +20,20 @@ const Card = ({ cardImage }) => {
             alt="fashion icon"
         />
         <CardContent sx={{color:"black"}}>
-        <Typography gutterBottom variant="body1" align="center" component="div" color={"tomato"}>
-            Fashion Icon
-        </Typography>
-        <Typography gutterBottom variant="h5" align="center" component="div" >
-            Be yourself and make everyone love you
-        </Typography>
-        <Typography gutterBottom variant="body2" align="center" component="div" >
-            It's women love
-        </Typography>
-        <Typography variant="body1" align="center" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-        </Typography>
+            <ThemeProvider theme={theme}>
+                <Typography gutterBottom variant="body1" align="center" component="div" color={"tomato"}>
+                    Fashion Icon
+                </Typography>
+                <Typography gutterBottom variant="h5" align="center" component="div" >
+                    Be yourself and make everyone love you
+                </Typography>
+                <Typography gutterBottom variant="body2" align="center" component="div" >
+                    It's women love
+                </Typography>
+                <Typography variant="body1" align="center" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                </Typography>
+            </ThemeProvider>
         </CardContent>
         </Link>
     </Box>
