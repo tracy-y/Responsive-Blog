dbcd1e17aed41b4c86da06bd752bc3eb416441fa
Tracey <tracy.yan2018@gmail.com>
2022-09-30 21:38:19 +1000

    Update Recent.js

--- ui/src/components/recent/Recent.js
+++ ui/src/components/recent/Recent.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React from "react";
 import cardImage from "../../static/card.jpg";
 import postImage1 from "../../static/image1.jpg";
 import postImage2 from "../../static/image2.jpg";
 import postImage3 from "../../static/image3.jpg";
 import postImage4 from "../../static/image4.jpg";
 import { Box, Grid } from "@mui/material";
-import Card from '../card/Card.js';
+import Card from "../card/Card.js";
 
 const Recent = () => {
   return (
     <Box>
-        <Grid container rowSpacing={1} columnSpacing={{xs:1, sm:2, md:1}}>
-            <Grid item >
-              <Card cardImage={cardImage} />
-            </Grid>
-            <Grid item md={6} xs={12}>
-                <Card cardImage={postImage1}/>
-            </Grid>
-            <Grid item md={6} xs={12}>
-                <Card cardImage={postImage2}/>
-            </Grid>
-            <Grid item md={6} xs={12}>
-                <Card cardImage={postImage3}/>
-            </Grid>
-            <Grid item md={6} xs={12}>
-                <Card cardImage={postImage4}/>
-            </Grid>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
+        <Grid item>
+          <Card cardImage={cardImage} />
         </Grid>
+        <Grid item md={6} xs={12}>
+          <Card cardImage={postImage1} />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Card cardImage={postImage2} />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Card cardImage={postImage3} />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Card cardImage={postImage4} />
+        </Grid>
+      </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default Recent
+export default Recent;
